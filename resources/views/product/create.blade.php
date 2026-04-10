1636c60de66ea94fbad67bead443657b42a0f255
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2026-04-10 17:12:36 +0700

    mengganti warna button save product menjadi hitam

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -100,7 +100,7 @@
                                 Cancel
                             </a>
                             <button type="submit" 
-                                    class="px-6 py-2.5 bg-indigo-600 hover:bg-indigo-700 text-white text-sm font-medium rounded-lg shadow-sm transition">
+                                    class="px-6 py-2.5 bg-indigo-600 hover:bg-indigo-700 text-black text-sm font-medium rounded-lg shadow-sm transition">
                                 Save Product
                             </button>
                         </div>
