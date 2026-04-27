f38b685dfbba6dd3304e1caf5273c37ca7440b26
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2026-04-27 18:19:50 +0700

    Memperbaiki skema migrasi kategori dengan menambahkan kolom unik untuk nama kategori

--- database/migrations/2026_03_07_034039_create_categories_table.php
+++ database/migrations/2026_03_07_034039_create_categories_table.php
@@ -9,15 +9,15 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+  
     public function up(): void
-    {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('categories', function (Blueprint $table) {
+        $table->id();
+        $table->string('name')->unique(); // Tambahkan unique agar tidak ada nama ganda
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
