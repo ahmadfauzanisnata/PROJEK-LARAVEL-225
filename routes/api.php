c530b848cb43ba9235103a0bd77b39b6382df24a
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2026-05-16 07:34:49 +0700

    Add API routes for user authentication and product management

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+   // Rute CRUD Category
+    Route::apiResource('categories', CategoryController::class);
+    
+    // Rute Product (Melengkapi yang sudah ada)
+    Route::post('/product', [ProductController::class, 'store']);
+    Route::put('/product/{id}', [ProductController::class, 'update']);
+    Route::delete('/product/{id}', [ProductController::class, 'destroy']);
+    
+});
+
+Route::get('/product/{id}', [ProductController::class, 'show']);
+
+Route::post('/login', [AuthController::class, 'getToken']);
+
+
+
+
