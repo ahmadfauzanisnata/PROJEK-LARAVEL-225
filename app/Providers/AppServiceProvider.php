dbe5aa943bb1a8e68f09db1b28caf8449fc53cc9
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2026-05-16 07:35:39 +0700

    Configure Scramble for API route handling and define viewApiDocs gate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Dedoc\Scramble\Scramble;
+use Illuminate\Routing\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +18,15 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        Scramble::configure()
+         ->routes(function (Route $route) {
+        return Str::startsWith($route->uri, 'api/');
+        });
+
+        Gate::define('viewApiDocs', function () {
+        return true;
+        });
+
         // Gate untuk fitur Export Product
         Gate::define('export-product', function (User $user) {
             return $user->role === 'admin';
